8076c6c925e8cea7ac8263a9be0ed8da4d346874
H@di <hadi@hadisafari.ir>
2021-09-20 16:56:54 +0430

    handle paths with space

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ if [ $# -ne 2 ]; then
     echo -e "usage:\t$0 <mmtp_output_file> <actual_output_dir>"
     exit 2
 fi
-FEED=$1
-DIR=$2
+FEED="$1"
+DIR="$2"
 
 read -p "Have you generated $FEED? [(y)/n] " i
 if [[ $i == "n" ]]; then
-    echo "Please generate $FEED and retry."
+    echo "Please generate "$FEED" and retry."
     exit
 fi
 
@@ -17,7 +17,7 @@ echo ""
 echo -e "\e[1m\e[33mPopulating ./raw...\e[39m\e[0m"
 rm -rf raw
 mkdir -p raw
-./splitter.py $FEED raw
+./splitter.py "$FEED" raw
 
 echo ""
 echo -e "\e[1m\e[33mPopulating ./feed & ./oracle...\e[39m\e[0m"
@@ -36,4 +36,4 @@ fi
 
 echo ""
 echo -e "\e[1m\e[33mPopulating ./res...\e[39m\e[0m"
-./preprocess_test_suite.sh $DIR
+./preprocess_test_suite.sh "$DIR"
